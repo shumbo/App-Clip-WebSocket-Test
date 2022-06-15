57bcee4e167e9a7385a6442ccad4a2dd82b3f364
Shun <shunthedev@gmail.com>
2022-06-15 08:05:43 -0700

    Initial Commit

---
+++ App-Clip-WebSocket-Test/App_Clip_WebSocket_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Clip_WebSocket_TestApp.swift
+//  App-Clip-WebSocket-Test
+//
+//  Created by Shun Kashiwa on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct App_Clip_WebSocket_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Clip-WebSocket-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App-Clip-WebSocket-Test
+//
+//  Created by Shun Kashiwa on 2022/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
